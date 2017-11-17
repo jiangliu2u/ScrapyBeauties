8a86a0224503cd6d8f5be9c17b840750774828c7
jiangliu <jiangliu2u@gmail.com>
2017-11-17 15:57:26 +0800

    Update NovelSpider.py

--- spiders/NovelSpider.py
+++ spiders/NovelSpider.py
@@ -10,25 +10,24 @@ class MySpider(scrapy.Spider):
     bashurl = '.html'
     f = open('D:/Novel_Info.txt','w+')
     def start_requests(self):
-        
         for i in range(1, 11):
             url = self.bash_url + str(i) + '_1' + self.bashurl
             yield Request(url, self.parse)
         yield Request('http://www.x23us.com/quanben/1', self.parse)
  
     def parse(self, response):
-		max_num = BeautifulSoup(response.text,'lxml').find('div',class_='pagelink').find_all('a')[-1].get_text()
-		bashurl = str(response.url)[:-7]
-		for num in range(1, int(max_num)+1):
-                    url = bashurl + '_' + str(num) +self.bashurl
-                    yield Request(url,callback = self.get_name)
+	max_num = BeautifulSoup(response.text,'lxml').find('div',class_='pagelink').find_all('a')[-1].get_text()
+	bashurl = str(response.url)[:-7]
+	for num in range(1, int(max_num)+1):
+        	url = bashurl + '_' + str(num) +self.bashurl
+                yield Request(url,callback = self.get_name)
 	
     def get_name(self, response):
-		tds = BeautifulSoup(response.text,'lxml').find_all('tr',bgcolor = '#FFFFFF')
-		for td in tds:
-			novelname = td.find('a',attrs={'target':'_blank'}).get_text()
-			novelurl = td.find('a')['href']
-			self.f.write(novelname.encode('utf-8')+': '+novelurl+'\n')
-			print novelname,': ',novelurl
+	tds = BeautifulSoup(response.text,'lxml').find_all('tr',bgcolor = '#FFFFFF')
+	for td in tds:
+		novelname = td.find('a',attrs={'target':'_blank'}).get_text()
+		novelurl = td.find('a')['href']
+		self.f.write(novelname.encode('utf-8')+': '+novelurl+'\n')
+		print novelname,': ',novelurl
 	
 	
